75b533eed50be7f935f6d73c9e715c094ea53ddd
Corey's iMac <coreyearleon@icloud.com>
2020-11-04 08:52:09 -0500

    Updated Parse-Swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/carekit-apple/CareKit.git", .branch("main")),
-        .package(url: "https://github.com/parse-community/Parse-Swift.git", .branch("improve_ci")),
+        .package(url: "https://github.com/parse-community/Parse-Swift.git", .branch("main")),
     ],
     targets: [
         .target(
