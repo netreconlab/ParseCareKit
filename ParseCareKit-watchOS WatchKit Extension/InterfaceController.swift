71dcdba585f53bf993ddb5018bbf41e99d04cf2a
Corey <coreyearleon@icloud.com>
2020-12-19 18:28:02 -0500

    Create ci.yml (#84)
    * Create ci.yml
    
    * Delete swift.yml
    
    * add tests
    
    * fix ci
    
    * update lockfile
    
    * remove pod folder
    
    * update git ignore
    
    * fix watchOS build
    
    * fix watchOS build
    
    * update badge in README
    
    * change file name of actions for this branch
    
    * Switch badges around
    
    * Update description in README

---
+++ ParseCareKit-watchOS WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,27 @@
+//
+//  InterfaceController.swift
+//  ParseCareKit-watchOS WatchKit Extension
+//
+//  Created by Corey Baker on 12/17/20.
+//  Copyright © 2020 Network Reconnaissance Lab. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
