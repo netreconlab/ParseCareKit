3d4e8fe2928a0ebfd397498dc6271c5990ddb892
Corey's iMac <coreyearleon@icloud.com>
2021-03-27 13:29:49 -0400

    Update ParseSwift and CareKit

--- Package.swift
+++ Package.swift
@@ -13,9 +13,9 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(name: "CareKit", url: "https://github.com/gavirawson-apple/CareKit",
-                 .revision("833fe88e7e96884a52ed173ed4a2a7875a49974d")),
-        .package(name: "ParseSwift", url: "https://github.com/parse-community/Parse-Swift", from: "1.2.2")
+        .package(name: "CareKit", url: "https://github.com/carekit-apple/CareKit",
+                 .revision("0c0cbe3abd2f0a1fd788a857f137d1726f0940f2")),
+        .package(name: "ParseSwift", url: "https://github.com/parse-community/Parse-Swift", from: "1.2.4")
     ],
     targets: [
         .target(
