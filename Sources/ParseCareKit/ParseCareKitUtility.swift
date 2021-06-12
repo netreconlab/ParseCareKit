f9ea0f747652196eb44d93adc9ddcb635f6f3a00
Corey <coreyearleon@icloud.com>
2021-06-12 10:03:33 -0400

    Add more documentation (#116)
    * Add more documentation
    
    * Update CI cache
    
    * Update readme

--- Sources/ParseCareKit/ParseCareKitUtility.swift
+++ Sources/ParseCareKit/ParseCareKitUtility.swift
@@ -9,15 +9,31 @@
 import Foundation
 import ParseSwift
 
+// swiftlint:disable line_length
+
 /// Utility functions designed to make things easier.
 public struct ParseCareKitUtility {
 
-    /// Can setup a connection to Parse Server based on a ParseCareKit.plist
+    /** Can setup a connection to Parse Server based on a ParseCareKit.plist file.
+    
+   The key/values supported in the file are a dictionary named `ParseClientConfiguration`:
+    - Server - (String) The server URL to connect to Parse Server.
+    - ApplicationID - (String) The application id of your Parse application.
+    - ClientKey - (String) The client key of your Parse application.
+    - LiveQueryServer - (String) The live query server URL to connect to Parse Server.
+    - UseTransactionsInternally - (Boolean) Use transactions inside the Client SDK.
+    - parameter authentication: A callback block that will be used to receive/accept/decline network challenges.
+     Defaults to `nil` in which the SDK will use the default OS authentication methods for challenges.
+     It should have the following argument signature: `(challenge: URLAuthenticationChallenge,
+     completionHandler: (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) -> Void`.
+     See Apple's [documentation](https://developer.apple.com/documentation/foundation/urlsessiontaskdelegate/1411595-urlsession) for more for details.
+     */
     public static func setupServer(authentication: ((URLAuthenticationChallenge,
                                                      (URLSession.AuthChallengeDisposition,
                                                       URLCredential?) -> Void) -> Void)? = nil) {
         var propertyListFormat =  PropertyListSerialization.PropertyListFormat.xml
         var plistConfiguration: [String: AnyObject]
+        var clientKey: String?
         var liveQueryURL: URL?
         var useTransactionsInternally = false
         guard let path = Bundle.main.path(forResource: "ParseCareKit", ofType: "plist"),
@@ -41,6 +57,10 @@ public struct ParseCareKitUtility {
                 fatalError("Error in ParseCareKit.setupServer(). Missing keys in \(plistConfiguration)")
         }
 
+        if let client = parseDictionary["ClientKey"] as? String {
+            clientKey = client
+        }
+
         if let liveQuery = parseDictionary["LiveQueryServer"] as? String {
             liveQueryURL = URL(string: liveQuery)
         }
@@ -50,6 +70,7 @@ public struct ParseCareKitUtility {
         }
 
         ParseSwift.initialize(applicationId: appID,
+                              clientKey: clientKey,
                               serverURL: serverURL,
                               liveQueryServerURL: liveQueryURL,
                               useTransactionsInternally: useTransactionsInternally,
