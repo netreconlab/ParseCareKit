3e9c5c37a52f763c9bc06a3812aa96205d88cb75
Corey Baker <coreyearleon@icloud.com>
2022-08-24 22:02:44 -0400

    another update

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .package(url: "https://github.com/cbaker6/CareKit.git",
                  revision: "adca4ac261b265e4fb7ded5a88e14deaed39592c"),
         .package(url: "https://github.com/cbaker6/Parse-Swift.git",
-                 revision: "e0563bf29ee87c728d909445a9561d257e1930b6")
+                 revision: "d492f93c1ffebb0dbec51bb161925fed9b1e809c")
         // .package(url: "https://github.com/parse-community/Parse-Swift.git",
         //          .upToNextMajor(from: "4.9.0"))
     ],
