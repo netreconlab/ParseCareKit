859dc9e854a95ea3006a3b0acdf19ec5d12fcb9a
Corey Baker <coreyearleon@icloud.com>
2024-07-10 19:09:42 -0700

    update packages

--- Package.swift
+++ Package.swift
@@ -12,9 +12,9 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/cbaker6/CareKit.git",
-            .upToNextMajor(from: "3.0.0-beta.2")),
+            .upToNextMajor(from: "3.0.0-beta.14")),
         .package(url: "https://github.com/netreconlab/Parse-Swift.git",
-            .upToNextMajor(from: "5.8.1"))
+            .upToNextMajor(from: "5.10.0"))
     ],
     targets: [
         .target(
